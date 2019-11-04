48290ed0b6d02461363b96c984b10f4851f0b31d
Qansa Alghifari <46017824+Qansa2916@users.noreply.github.com>
2019-11-04 10:57:59 +0700

    Tugas_26_JS

---
+++ Tugas-26.js
@@ -0,0 +1,60 @@
+function tugas26() {
+    var x = [2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16];
+    y = isFinite(x);
+
+
+
+
+    if (y == false) {
+        console.log("Angka ", x[0],
+            "NOT Infinity");
+        console.log("Angka ", x[1],
+            "NOT Infinity");
+        console.log("Angka ", x[2],
+            "NOT Infinity");
+        console.log("Angka ", x[3],
+            "NOT Infinity");
+        console.log("Angka ", x[4],
+            "NOT Infinity");
+        console.log("Angka ", x[5],
+            "NOT Infinity");
+        console.log("Angka ", x[6],
+            "NOT Infinity");
+        console.log("Angka ", x[7],
+            "NOT Infinity");
+        console.log("Angka ", x[8],
+            "NOT Infinity");
+        console.log("Angka ", x[9],
+            "NOT Infinity");
+        console.log("Angka ", x[10],
+            "NOT Infinity");
+
+    } else {
+        console.log("Angka ", x[0],
+            " Infinity");
+        console.log("Angka ", x[1],
+            " Infinity");
+        console.log("Angka ", x[2],
+            " Infinity");
+        console.log("Angka ", x[3],
+            " Infinity");
+        console.log("Angka ", x[4],
+            " Infinity");
+        console.log("Angka ", x[5],
+            " Infinity");
+        console.log("Angka ", x[6],
+            " Infinity");
+        console.log("Angka ", x[7],
+            " Infinity");
+        console.log("Angka ", x[8],
+            " Infinity");
+        console.log("Angka ", x[9],
+            " Infinity");
+        console.log("Angka ", x[10],
+            " Infinity");
+
+    }
+
+
+}
+tugas26();
